5694499f27818985820f037450c3208a8a3bad2e
Rishitha450 <rishitharangu80@gmail.com>
2026-04-03 00:31:07 +0530

    Day-6-statelocking/

---
+++ Day-6-statelocking/main.tf
@@ -0,0 +1,36 @@
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "prod"
+    }
+  
+}
+
+resource "aws_subnet" "name" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = "10.0.0.0/24"
+    tags = {
+        Name = "prod-subnet"
+    }
+  
+}
+
+
+
+resource "aws_subnet" "name-1" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = "10.0.1.0/24"
+    tags = {
+        Name = "prod-subnet"
+    }
+  
+}
+
+resource "aws_subnet" "name-2" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = "10.0.2.0/24"
+    tags = {
+        Name = "prod-subnet-1"
+    }
+  
+}
